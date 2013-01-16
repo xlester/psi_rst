633919a84329c67c1c4930dd0f1a13f9408a4842
Orlando Medina <orlando@Humboldt.(none)>
2013-01-16 13:43:52 -0600

    Se agrego funcion para calcular la desviacion standar. En esta funcion se quita el piston de fase automaticamente. Las pruebas hasta el momento demuestran que el RST es mejor que el AIA.

---
+++ PhaseStd.m
@@ -0,0 +1,16 @@
+function [var]=PhaseStd(realPhase,estPhase)
+[M N] = size(realPhase);
+
+
+
+dif = angle( exp( 1i*(estPhase-realPhase) ) );
+piston = sum(dif(:))/(M*N);
+
+estPhase = angle( exp( 1i*(estPhase-piston) ) );
+
+dif = angle( exp( 1i*(estPhase-realPhase) ) );
+var = sqrt(sum(sum(dif.^2))/(M*N));
+
+
+
+end
